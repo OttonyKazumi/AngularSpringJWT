dab9f5904ccc96fe501a37c38198898e558e0e9d
OttonyKazumi <ottony@hotmail.com>
2024-11-19 18:00:57 -0300

    profile updated

--- Angular/src/app/page/profile/profile.component.ts
+++ Angular/src/app/page/profile/profile.component.ts
@@ -1,23 +1,45 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthServiceService } from '../../service/auth-service.service';
+import {Component, OnInit} from '@angular/core';
+import {NgIf} from "@angular/common";
+import {AuthServiceService} from "../../service/auth-service.service";
+import {CardModule} from "primeng/card";
+import {Button} from "primeng/button";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    CardModule,
+    Button
+  ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent {
-  constructor(private _router: Router, private _service: AuthServiceService){}
+export class ProfileComponent implements OnInit{
+  user: any;
+  role: any;
 
+
+  constructor(private authService: AuthServiceService, private router: Router) {}
+
+  ngOnInit(): void {
+
+    this.authService.getUserDetails().subscribe({
+      next: (data) => {
+        console.log('User details:', data);
+        this.user = data;
+        this.role = this.user.authorities.map((auth: { authority: string }) => auth.authority);
+      },
+      error: (error) => {
+        console.error('Erro ao obter detalhes do usuário:', error);
+        if (error.status === 401) {
+          alert('Autenticação falhou. Faça login novamente.');
+        }
+      },
+    });
+  }
   home(){
-    this._router.navigate(['home'])
+    this.router.navigate(['home'])
   }
-
-  logout(){
-    this._service.logout();
-  }
-
 }
